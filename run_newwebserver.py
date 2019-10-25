19c61571ceb9ea9ef78d41a90a63a9f23f56268f
Niall Jordan <niall.jordan2012@gmail.com>
2019-10-25 21:10:17 +0100

    Url Image Loading with metadata on html

--- run_newwebserver.py
+++ run_newwebserver.py
@@ -6,10 +6,12 @@ import logging
 import requests
 import subprocess
 import logging
+from progress.bar import IncrementalBar
 from botocore.exceptions import  ClientError
 from datetime import datetime,timedelta
 
-logging.basicConfig(level=logging.INFO)
+logging.basicConfig(filename='info.log', filemode='w', level=logging.INFO, format='%(asctime)s %(message)s')
+
 ec2 = boto3.resource('ec2', region_name='eu-west-1')
 s3 = boto3.resource('s3')
 cloudwatch = boto3.resource('cloudwatch',region_name='eu-west-1')
@@ -38,11 +40,13 @@ def create_instance(image_id,inst_type,key):
         )
     except Exception as e:
         print(e)
+        logging.error(e)
 
     #Adding a tag to the instance
     name_tag ={'Key': 'Name', 'Value': 'Assignment Instance'}
     instance[0].create_tags(Tags= [name_tag])
     return instance[0]
+
 
 #------------------Create & Delete Bucket Method------------------#s
 def create_bucket(bucket_name):
@@ -50,15 +54,18 @@ def create_bucket(bucket_name):
         response = s3.create_bucket(Bucket=bucket_name, CreateBucketConfiguration={'LocationConstraint' : 'eu-west-1'})
         print(response)
     except:
+        print(f'ERROR: {bucket_name} already exists')
         logging.error(f'{bucket_name} already exists')
 
-def del_bucket_contents(bucket):
-    for key in bucket.objects.all():
-        try:
-            response = key.delete()
-            print(response)
-        except Exception as e:
-            print (e)
+#Delete bucket + contents
+# def del_bucket_contents(bucket):
+#     for key in bucket.objects.all():
+#         try:
+#             response = key.delete()
+#             print(response)
+#         except Exception as e:
+#             print(e)
+#             logging.error(e)
 
 #------------------Pulling and Pulling of image from url to bucket------------------#
 def pull_image(url,filename):
@@ -69,31 +76,34 @@ def pull_image(url,filename):
         output.close()
     except Exception as e:
         print(e)
+        logging.error(e)
 
 def push_image(filename,bucket):
     try:
-        s3.Object(bucket,filename).put(Body=open(filename,'rb'))
+        s3.Object(bucket,filename).put(Body=open(filename,'rb'),ACL='public-read')
     except Exception as e:
         print(e)
+        logging.error(e)
 
-def metricsForCloudwatch(instanceId):
-    metric_iterator = cloudwatch.metrics.filter(
+#Lab Code used for metrics (Ref)
+def metricForCloudwatch(instanceId,metricName):
+    metricInfo = cloudwatch.metrics.filter(
         Namespace='AWS/EC2',
-        MetricName='CPUUtilization',
+        MetricName= metricName,
         Dimensions=[{'Name':'InstanceId', 'Value': instanceId}])
 
-    for metric in metric_iterator:
+    for metric in metricInfo:
         response = metric.get_statistics(StartTime=datetime.now() - timedelta(minutes=65),     # 5 minutes ago
                                             EndTime=datetime.now() - timedelta(minutes=60),       # now
                                             Period=300,                                           # 5 minute intervals
                                             Statistics=['Average'])
-        print ("Average CPU utilisation:", response['Datapoints'][0]['Average'])
-        print (response)   # for debugging only
-
-
-
+        print (" >> Average Result of "+ metricName+" (%):", response['Datapoints'][0]['Average'])
+        logging.debug(response)   # for debugging only
 
 def main():
+
+    #Initiate logging
+    logging.info('Started Logging')
     #------------------Assigning values before running------------------#
     image_id ='ami-0ce71448843cb18a1'
     inst_type = 't2.micro'
@@ -106,68 +116,114 @@ def main():
 
     #------------------LAUNCH INSTANCE------------------#
     instance = create_instance(image_id,inst_type,key)
-    logging.info('>Instance has been created')
+    logging.info('Instance has been created')
+    print('  >> Instance has been created')
 
-    logging.info("Waiting for instance state = running")
-    instance.wait_until_running()
-    print (instance.id, instance.state)
+    print("  >> Waiting for instance state = running")
+    with IncrementalBar('  >> Launching',max=100) as bar:
+        for i in range(100):
+            time.sleep(1)
+            bar.next()
+    logging.info(instance.state)
+    print(instance.state)
+    print()
 
 
     #------------------CREATING BUCKET------------------#
-    logging.info('>Creating Bucket:')
+    logging.info('Creating Bucket:')
+    print(f'  >> Creating Bucket: {bucket}')
     time.sleep(5)
     create_bucket(bucket)
 
 
     #------------------Pulling Image from the url provided------------------#
+    logging.info("Pulling image from url")
     time.sleep(5)
     print()
-    logging.info('>Pulling image from url')
+    print('  >> Pulling image from url')
     pull_image(imageUrl, filename)
 
     print()
-    logging.info('>Image downloaded')
+    print('  >> Image downloaded')
     print()
 
 
     #------------------Pushing image to bucket------------------#
+    logging.info("Pushing Image to bucket")
     time.sleep(5)
-    logging.info('>Image is being pushed to the bucket')
+    print('  >> Image is being pushed to the bucket')
     push_image(filename,bucket)
 
     print()
-    logging.info(f'>Image is now in {bucket}' )
+    print(f'  >> Image is now in {bucket}' )
     print()
 
 
     #------------------Ssh remote command to retrieve------------------#
-    logging.info('>SSHing into ec2-user@'+ instance.public_ip_address )
-    logging.info("Please wait while we try to ssh into the instance")
-    time.sleep(60)
+    logging.info("Beginning ssh process")
+
+    print('  >> SSHing into ec2-user@'+ instance.public_ip_address )
+    print("  >> Please wait while we try to ssh into the instance")
+    with IncrementalBar('  >> SSHing into Instance',max=30) as bar:
+        for i in range(30):
+            time.sleep(1)
+            bar.next()
     instance.reload()
     print()
-    copy = "scp -i njordan-key.pem index.html ec2-user@"+instance.public_ip_address +":."
-    scopy = "ssh -o StrictHostKeyChecking=no -i njordan-key.pem ec2-user@"+instance.public_ip_address + " 'sudo cp index.html /var/www/html/index.html' "
-    line1 = "ssh -i  njordan-key.pem ec2-user@" + instance.public_ip_address  + " 'echo 'Instance Meta Data' >> /var/www/html/index.html'"
-    metadata = "ssh -i  njordan-key.pem ec2-user@" + instance.public_ip_address  + " 'curl http://169.254.169.254/latest/meta-data/local-ipv4 >> /var/www/html/index.html '"
 
-    logging.info(">Copying file from local machine to the EC2 instance")
-    subprocess.run(copy,shell=True)
-    logging.info(">Success: File Copied")
+    #linebreak
+    nextLine = "ssh -i njordan-key.pem ec2-user@"+instance.public_ip_address+ """ 'echo "</br>"  >> /var/www/html/index.html'"""
 
-    logging.info(">'index.html' is being copied to the /var/www/html folder on AWS")
-    subprocess.run(scopy,shell=True)
-    logging.info(">Success: File Copied")
+    #Adding metadata to index
+    logging.info("Adding metadata to index")
 
-    logging.info(">Instance Meta Data is being loaded to index.html")
-    subprocess.run(line1,shell=True)
-    subprocess.run(metadata,shell=True)
+    ipLine = "ssh  -o StrictHostKeyChecking=no -i  njordan-key.pem ec2-user@" + instance.public_ip_address  + " 'echo 'Private IP:' >> /var/www/html/index.html'"
+    ipmetadata = "ssh -i  njordan-key.pem ec2-user@" + instance.public_ip_address  + " 'curl http://169.254.169.254/latest/meta-data/local-ipv4 >> /var/www/html/index.html'"
+
+
+    print("  >> Instance Meta Data is being loaded to index.html")
+    subprocess.run(ipLine,shell=True)
+    subprocess.run(ipmetadata,shell=True)
+    subprocess.run(nextLine, shell=True)
+    print()
+
+    #Security Group Meta-data
+    secGroupLine = "ssh -i  njordan-key.pem ec2-user@" + instance.public_ip_address  + " 'echo 'Security Groups:' >> /var/www/html/index.html'"
+    secGroupMetadata = "ssh -i  njordan-key.pem ec2-user@" + instance.public_ip_address  + " 'curl http://169.254.169.254/latest/meta-data/security-groups >> /var/www/html/index.html'"
+    subprocess.run(secGroupLine,shell=True)
+    subprocess.run(secGroupMetadata,shell=True)
+    subprocess.run(nextLine, shell=True)
+    print()
+
+    #Hostname Meta-data
+    hostnameLine = "ssh -i  njordan-key.pem ec2-user@" + instance.public_ip_address  + " 'echo 'Hostname:' >> /var/www/html/index.html'"
+    hostnameMetadata = "ssh -i  njordan-key.pem ec2-user@" + instance.public_ip_address  + " 'curl http://169.254.169.254/latest/meta-data/hostname >> /var/www/html/index.html'"
+    subprocess.run(hostnameLine,shell=True)
+    subprocess.run(hostnameMetadata,shell=True)
+    subprocess.run(nextLine, shell=True)
+    print()
+
+
+    #Loading Image onto the page
+    logging.info("Attempting to display image")
+
+    imageInPage = "ssh -i njordan-key.pem ec2-user@" + instance.public_ip_address + f""" echo '\<img src="https://{bucket}.s3-eu-west-1.amazonaws.com/{filename} alt="Image for Assignment" \>"  >> /var/www/html/index.html' """
+    print("  >> Loading Image")
+    subprocess.run(imageInPage,shell=True)
+    print("  >> Success: Image Loaded")
+    print()
 
     #---------------------Cloudwatch & Metrics------------------#
+    logging.info("Cloudwatch Metrics")
     print()
-    logging.info(">Cloudwatch Metrics:")
-    metricsForCloudwatch(instance.id)
+    print("  >> Cloudwatch Metrics:")
+    metricForCloudwatch(instance.id,"CPUUtilization")
+    metricForCloudwatch(instance.id,"NetworkIn")
+    metricForCloudwatch(instance.id,"NetworkOut")
 
+    #Ending Logging
+    logging.info("Script finished successfully")
+    logging.info('Finished Logging')
 main()
 
 logging.shutdown
